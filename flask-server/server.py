43a8ba57ce27d3a7b01b891ae01289ffceba6634
Kuo-kuo <eric.kuo3658@gmail.com>
2022-08-03 15:44:21 -0700

    removed initial react scripts, beginning login page
    set up initial components and styling for login page, beginning code for backend oauth redirect

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/hello_world")
+class Login(Resource):
+    def get(self):
+        data = "Hello World"
+        return jsonify({'data' : data })
 
-def hello_world():
-    return {"message":"Hello World"}
+api.add_resource(Login, '/auth')
 
 if __name__ == "__main__":
     app.run(debug=True)
