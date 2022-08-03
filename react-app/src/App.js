e709cce87884a25f5e2a70f4474456b8b6ecbeaa
Sergio Peterson <sergiopeter2016@berkeley.edu>
2022-08-02 21:30:59 -0700

    made react app run on local host

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,30 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/hello_world").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
-
   return (
-    <div>
-      {(typeof data.message === 'undefined') ? (
-        <p> Backend broke </p>
-      ) : (
-        <p> {data.message} </p>
-      )
-      }
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is working !!!
+          -S.P
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
